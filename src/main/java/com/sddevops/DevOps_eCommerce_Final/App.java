66cddb8e30ee10d448a87944f718ab5ad348c466
sitaraanil <2103368C@student.tp.edu.sg>
2023-02-04 03:10:35 +0800

    Initial Commit

---
+++ src/main/java/com/sddevops/DevOps_eCommerce_Final/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.DevOps_eCommerce_Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
